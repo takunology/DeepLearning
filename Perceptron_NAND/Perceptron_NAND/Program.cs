b9c1c1611eec2271bbe3dc03d1e41a63fdfc2036
takunology <m15025@g.metro-cit.ac.jp>
2019-10-28 19:50:53 +0900

    単層パーセプトロン

---
+++ Perceptron_NAND/Perceptron_NAND/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Perceptron_NAND
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Perceptron NAND Logic.");
+
+                Console.Write("Input x1:");
+                var x1 = double.Parse(Console.ReadLine());
+                Console.Write("Input x2:");
+                var x2 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Output  :{NAND(x1, x2)}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public static int NAND(double x1, double x2)
+        {
+            //重みのパラメータ
+            var w1 = -1;
+            var w2 = -1;
+            var threshold = -1.5; // 閾値
+            var tmp = (x1 * w1) + (x2 * w2); //積の和
+
+            if (threshold < tmp) { return 1; } // 閾値を超えた場合
+            else { return 0; } // 閾値以下の場合
+        }
+    }
+}
---
+++ Perceptron_OR/Perceptron_OR/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Perceptron_OR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Perceptron OR Logic.");
+
+                Console.Write("Input x1:");
+                var x1 = double.Parse(Console.ReadLine());
+                Console.Write("Input x2:");
+                var x2 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Output  :{OR(x1, x2)}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public static int OR(double x1, double x2)
+        {
+            //重みのパラメータ
+            var w1 = 2;
+            var w2 = 2;
+            var threshold = 1; // 閾値
+            var b = 1; //バイアス
+            var tmp = b + (x1 * w1) + (x2 * w2); //積の和
+
+            if (threshold < tmp) { return 1; } // 閾値を超えた場合
+            else { return 0; } // 閾値以下の場合
+        }
+    }
+}
