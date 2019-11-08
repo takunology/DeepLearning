0fc924a429a8722e1c826a6e8cdfbcd1b0750271
takunology <m15025@g.metro-cit.ac.jp>
2019-11-08 20:50:37 +0900

    恒等関数

--- 3NN_NotLearningCLI/3NN_NotLearningCLI/NNFunction.cs
+++ 3NN_NotLearningCLI/3NN_NotLearningCLI/NNFunction.cs
@@ -6,21 +6,30 @@ namespace _3NN_NotLearningCLI
 {
     public class NNFunction
     {
+        //ステップ関数
         public double Step(double x)
         {
             if(x > 0) { return 1; }
             else { return 0; }
         }
         
+        //シグモイド関数
         public double Sigmoid(double x)
         {
             return 1 / (1 + Math.Exp(-x));
         }
 
+        //ReLU関数
         public double ReLU(double x)
         {
             if(x > 0) { return x; }
             else { return 0; }
+        }
+
+        //恒等関数
+        public double Identity(double x)
+        {
+            return x;
         }
     }
 }
