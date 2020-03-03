fda13f1fba55cb0a05b99c71585a392fc3051a13
takunology <m15025@g.metro-cit.ac.jp>
2020-03-03 21:45:28 +0900

    ソフトマックス関数の実装

---
+++ NeuralNetwork/3NN_ForwoardOnly/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _3NN_ForwardOnly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            int Neuron = 3; //ニューロンの数
+
+            Console.Write("入力層のニューロン数：");
+            Neuron = int.Parse(Console.ReadLine()); 
+            _ = new InputLayer(Neuron);//Neuron行1列として初期化
+
+            Console.Write("\n第1中間層のニューロン数：");
+            Neuron = int.Parse(Console.ReadLine());
+            _ = new MiddleLayer(Neuron);//Neuron列として初期化
+
+            Console.Write("\n第2中間層のニューロン数：");
+            Neuron = int.Parse(Console.ReadLine());
+            _ = new MiddleLayer(Neuron);//Neuron列として初期化
+
+            Console.Write("\n出力層のニューロン数：");
+            Neuron = int.Parse(Console.ReadLine());
+            _ = new OutputLayer(Neuron);//Neuron列として初期化
+
+            Console.ReadKey();
+        }
+    }
+}
